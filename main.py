e0897579eba5fda369cb3a89abbe1ff4358c285c
Daipayan Neogi <draven@Draven.local>
2024-02-07 21:47:11 +0530

    girls dummy data

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def read_root():
 @app.get("/girls")
 def read_root():
     return {
-        "BoysHostelMenu": [final_girlsMenu,final_girlsMenu,final_special_menu]
+        "BoysHostelMenu": [final_girlsMenu,final_girlsMenu,final_girlsMenu]
     }
 
 
